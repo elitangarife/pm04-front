eb2b0226dea9bc476d2bdfe2bf42d42539dadb24
elitangarife <elianatangarifeblandon@gmail.com>
2025-04-23 11:56:34 -0500

    upd:subnavbar

--- src/components/SubNavbar/SubNavbar.tsx
+++ src/components/SubNavbar/SubNavbar.tsx
@@ -1,14 +1,11 @@
 
-import React from 'react'
+import React from 'react
 import Link from 'next/link'
 
 
 const SubNavbar = () => {
-  
-  
   return (
-
-    <div className="bg-gray-200 w-full mx-auto space-x-4 font-semibold flex items-center justify-between px-4 py-2">
+    <div className="bg-gray-200 w-full mx-auto font-semibold flex items-center justify-between px-4 py-2">
       
       {/* Menú hamburguesa visible en móviles */}
       <div className="sm:hidden">
@@ -19,8 +16,8 @@ const SubNavbar = () => {
         </button>
       </div>
 
-      {/* Menú de enlaces */}
-      <div className="hidden sm:flex space-x-6 text-xs sm:text-base md:text-lg">
+      {/* Menú de enlaces, centrado en pantallas grandes */}
+      <div className="hidden sm:flex sm:justify-center w-full space-x-6 text-xs sm:text-base md:text-lg">
         <Link className="text-gray-900 hover:text-gray-400" href="/products/1">Smartphones</Link>
         <Link className="text-gray-900 hover:text-gray-400" href="/products/2">Laptops</Link>
         <Link className="text-gray-900 hover:text-gray-400" href="/products/3">Tablets</Link>
@@ -30,22 +27,7 @@ const SubNavbar = () => {
         <Link className="text-gray-900 hover:text-gray-400" href="/products/7">Accessories</Link>
       </div>
     </div>
-    // <div className="bg-gray-200 w-full mx-auto space-x-4 font-semibold flex items-center justify-around">
-        
-            
-    //     <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6 text-xs sm:text-base md:text-lg">             
-                
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/1">Smartphones</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/2">Laptops</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/3">Tablets</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/4">Headphones</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/5">Cameras</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/6">Printers</Link>
-    //               <Link className="text-gray-900 hover:text-gray-400" href="/products/7">Accessories</Link>           
+  );
+};
 
-    //     </div>
-    // </div>
-  )
-}
-
-export default SubNavbar
+export default SubNavbar;
