75ba782a368e668e17deb7c610491d48bf550afa
elitangarife <elianatangarifeblandon@gmail.com>
2025-04-25 10:06:27 -0500

    upd

--- src/helpers/categories.ts
+++ src/helpers/categories.ts
@@ -1,13 +1,13 @@
 import { ICategory } from "@/utils";
 
 const categoriesToPreLoad: ICategory[] = [
-        { name: 'Smartphones', id:1 },
-        { name: 'Laptops', id: 2 },
-        { name: 'Tablets', id: 3 },
-        { name: 'Headphones', id: 4 },
-        { name: 'Cameras', id: 5 },
-        { name: 'Printers', id: 6 },
-        { name: 'Accessories', id: 7 },
+    { name: 'Celulares', id:1 },
+    { name: 'Portátiles', id: 2 },
+    { name: 'Tabletas', id: 3 },
+    { name: 'Auriculares', id: 4 },
+    { name: 'Cámeras', id: 5 },
+    { name: 'Impresoras', id: 6 },
+    { name: 'Accessorios', id: 7 },
     ]
 
     export default categoriesToPreLoad
